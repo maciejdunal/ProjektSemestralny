0d53ae04e0f7038212530bd40b85c7cca59bd40a
Maciej Dunal <maciej.dunal@microsoft.wsei.edu.pl>
2021-02-03 00:25:34 +0100

    Login window added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,17 +13,61 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace ProjektSemestralny
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Login window initialization
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
+
+        /// <summary>
+        /// Method performing login to the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            downloadData();
+        }
+
+        /// <summary>
+        /// Method connecting to the database
+        /// </summary>
+        public void downloadData()
+        {
+            string myConnection =
+                "SERVER=" + serverNametb.Text + ";" +
+                "DATABASE=" + databaseNametb.Text + ";" +
+                "UID=" + logintb.Text + ";" +
+                "PASSWORD=" + passwordtb.Password + ";";
+
+            SqlConnection connection = new SqlConnection(myConnection);
+
+                try
+                {
+                    connection.Open();
+                    var Nextwindow = new Window1();
+                    Nextwindow.ShowDialog();
+                }
+
+            ///<exception cref="SqlException">Thrown when fail logging.</exception>
+            catch (SqlException ex)
+                {
+                    MessageBox.Show("Błąd logowania do bazy danych", "Błąd");
+                }
+                connection.Close();
+        }
+     }
 }
+
