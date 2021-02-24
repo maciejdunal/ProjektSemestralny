b554831d147d3be32174f3168ca997ce4926403b
maciejdunal <72334603+maciejdunal@users.noreply.github.com>
2021-02-24 23:25:02 +0100

    LoginTests added.

---
+++ ProjektSemestralnyUnitTests/LoginTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjektSemestralny;
+
+namespace ProjektSemestralnyUnitTests
+{
+    [TestClass]
+    public class LoginTests
+    
+    {
+        [DataTestMethod]
+        [DataRow("user", "1234")]
+ 
+        public void AssertCredentialsToConnectionString(string user, string pass)
+        {
+            var credentials = DatabaseService.AddCredentials(user, pass);
+            var expected = $"{DatabaseService.connectionStringSSA}User ID={user};Password={pass}";
+            Assert.AreEqual(credentials, expected);
+        }
+
+        [DataTestMethod]
+        [DataRow("user", "1234")]
+        public void AssertSuccessfulSSALogin(string user, string pass)
+        {
+            Assert.AreEqual(true, DatabaseService.OpenConnectionSSA(user, pass), "user/1234 are correct credentials");
+        }
+
+        [DataTestMethod]
+        [DataRow("user", "123423212312")]
+        public void AssertUnsuccessfulSSALogin(string user, string pass)
+        {
+            Assert.AreEqual(false, DatabaseService.OpenConnectionSSA(user, pass), "user/1234 are incorrect credentials");
+        }
+    }
+}
